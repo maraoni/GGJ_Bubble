b3b0d219759aca33a562c92ed7fbaf35f1884c66
maraon99 <90472204+maraon99@users.noreply.github.com>
2025-01-24 16:42:10 +0100

    Machinetext

---
+++ Assets/Tech/Scripts/UI/MachineText.cs
@@ -0,0 +1,34 @@
+using TMPro;
+using UnityEngine;
+
+public class MachineText : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI myText;
+    [SerializeField] private string DebugText;
+    [HideInInspector] public bool IsTyping = false;
+
+
+    [ContextMenu("Display DebugText")]
+    public void DisplayText()
+    {
+        _ = ShowText(DebugText);
+    }
+
+    public void DisplayText(string someText)
+    {
+        _ = ShowText(someText);
+    }
+
+    private async Awaitable ShowText(string someText)
+    {
+        IsTyping = true;
+        string totalText = "";
+        foreach (char c in someText)
+        {
+            await Awaitable.WaitForSecondsAsync(0.03f);
+            totalText += c;
+            myText.text = totalText;
+        }
+        IsTyping = false;
+    }
+}
