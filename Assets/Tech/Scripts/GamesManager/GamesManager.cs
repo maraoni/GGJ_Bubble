38bfe26d04d7f6f5cfa49eae7b3f2cf665b21863
maraon99 <90472204+maraon99@users.noreply.github.com>
2025-01-25 15:55:04 +0100

    Intro state n main menu

--- Assets/Tech/Scripts/GamesManager/GamesManager.cs
+++ Assets/Tech/Scripts/GamesManager/GamesManager.cs
@@ -15,6 +15,8 @@ public class GamesManager : StateMachine
     private void Start()
     {
         InitializeStateMachine();
+
+        SwitchState<MainMenu>();
     }
     public void DisplayText(string someText)
     {
